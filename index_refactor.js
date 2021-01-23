48f9800e9ada790a2bf3cf3461571ff2ee92d5e9
A2va <49582555+A2va@users.noreply.github.com>
2021-01-23 16:50:44 +0100

    Load all command recursively

--- index_refactor.js
+++ index_refactor.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const config = require('./config.json');
+const glob = require('glob')
 
 const client = new Discord.Client();
 const fs = require("fs")
@@ -11,6 +12,7 @@ client.aliases = new Discord.Collection();
 
 //Load the events
 fs.readdir("./events/", (err, files) => {
+
   files.forEach((file) => {
     const eventHandler = require(`./events/${file}`);
     const eventName = file.split(".")[0];
@@ -18,17 +20,26 @@ fs.readdir("./events/", (err, files) => {
   });
 });
 
+//Function for get all files into directory
+var recursive_readdir = function (src, callback) {
+  glob(src + '/**/*', callback);
+};
+
 //Load the commands
-fs.readdir("./command_refactor/", (err, files) => {
-  files.filter(file => file.endsWith('.js')).forEach((file) => {
-    const command = require(`./command_refactor/${file}`);
-    client.commands.set(command.name,command);
+recursive_readdir('command_refactor', function (err, files) {
+  if (err) {
+    console.log('Error', err);
+  } else {
+    files.filter(file => file.endsWith('.js')).forEach((file) => {
+      const command = require(`./${file}`);
+      client.commands.set(command.name, command);
 
-    if (command.aliases) {
-      command.aliases.forEach(alias => {
+      if (command.aliases) {
+        command.aliases.forEach(alias => {
           client.aliases.set(alias, command);
-      });
-  };
-  });
-});
+        });
+      }
+    });
+  }
 
+});

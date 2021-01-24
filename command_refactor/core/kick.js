b753c742833e8118e2cdd856df17552972a9c4ef
A2va <49582555+A2va@users.noreply.github.com>
2021-01-24 14:34:04 +0100

    Add kick command

---
+++ command_refactor/core/kick.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: 'kick',
+    aliases: [],
+    description: 'Kick a person',
+    category: 'Core',
+    utilisation: '{prefix}kick',
+    execute(client, message, args) {
+
+        if (!message.member.hasPermission("ADMINISTRATOR")) return;
+
+        let mention = message.mentions.members.first();
+
+        if (mention == undefined) {
+            message.reply("Erreur : mention du membre incomplète ou inexacte.");
+        }
+        else {
+            if (mention.kickable) {
+                mention.kick();
+                message.channel.send(mention.displayName + " a été(e) kické(e) avec succès !");
+            }
+            else {
+                message.reply(" Impossible de kicker ce membre.");
+            }
+        }
+    },
+};

05a5d73ad3b1a9448ef6aba5f807145ab4018acb
A2va <49582555+A2va@users.noreply.github.com>
2021-01-25 18:42:44 +0100

    Add lyrics command

---
+++ command_refactor/music/lyrics.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+module.exports = {
+    name: 'lyrics',
+    aliases: [],
+    description: '',
+    category: 'Music',
+    utilisation: '{prefix}lyrics',
+    execute(client, message, args) {
+        //const args = message.content.slice(prefix.length + (6)).trim().split(/ +/g);
+        if (!args[0]) {
+            const embed = new Discord.MessageEmbed()
+                .setColor('RED')
+                .setTitle(':x: Song title not provided!')
+            return message.channel.send(embed)
+        }
+        const lyrics = await lyricsFinder(args.join(' ')) || "Not Found!";
+        for (let i = 0; i < lyrics.length; i += 2000) {
+            const toSend = lyrics.substring(i, Math.min(lyrics.length, i + 2000));
+            const embed = new Discord.MessageEmbed()
+                .setColor('RANDOM')
+                .setDescription(toSend)
+            message.channel.send(embed).catch(e => {
+                console.log(e)
+            });
+        }
+    },
+};

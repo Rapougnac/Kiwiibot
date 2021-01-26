664ef7f31035715d80ea1ab91168150b6beefd4e
A2va <49582555+A2va@users.noreply.github.com>
2021-01-26 21:40:16 +0100

    Add 3000y command

--- command_refactor/misc/3000y.js
+++ command_refactor/misc/3000y.js
@@ -2,21 +2,21 @@ const Discord = require("discord.js");
 const AmeClient = require('amethyste-api');
 
 module.exports = {
-    name: 'facebook',
+    name: '3000y',
     aliases: [],
     description: '',
     category: 'Misc',
-    utilisation: '{prefix}facebook',
+    utilisation: '{prefix}3000y',
     execute(client, message, args) {
         const AmeAPI = new AmeClient('666703a31e32bb7b294a531293711476c207b7b9949aa1cc5c8892fc2475dfb8f521d832e47974d4f7c0e83bebaaadc3ad351c3e1ec8837fab4b3cecccf0b139'); {
 
             //const args = message.content.trim().split(/ +/g);
             const User = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase().includes() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase().includes() === args.join(' ').toLocaleLowerCase())
-                || message.member; let m = await message.channel.send("**Please Wait...**");
-            const buffer = await AmeAPI.generate("facebook", { url: User.user.displayAvatarURL({ format: "png", size: 2048, dynamic: true }) });
-            const attachment = new Discord.MessageAttachment(buffer, "facebook.png");
+              || message.member; let m = await message.channel.send("**Please Wait...**");
+            const buffer = await AmeAPI.generate("3000years", { url: User.user.displayAvatarURL({ format: "png", size: 2048 }) });
+            const attachment = new Discord.MessageAttachment(buffer, "3000years.png");
             m.delete({ timeout: 5000 });
             message.channel.send(attachment);
-        }
+          }
     },
 };

70043b6bd13c054f1f726c4923a8619fdc084859
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 22:04:01 +0100

    Fix async function

--- commands/misc/akaneko/maid.js
+++ commands/misc/akaneko/maid.js
@@ -1,6 +1,10 @@
 const akaneko = require('akaneko');
 const Discord = require("discord.js");
 
+async function yourFunctionName() {
+	let img = await akaneko.nsfw.maid();
+	return img;
+}
 module.exports = {
 	name: 'maid',
 	aliases: [],
@@ -8,9 +12,12 @@ module.exports = {
 	category: 'Misc',
 	utilisation: '{prefix}maid',
 	execute(client, message, args) {
-		const emebed = new Discord.MessageEmbed()
-			.setTitle(`${message.author.tag} here some maids (Maids, Maid Uniforms, etc, you know what maids are :3)`)
-			.setImage( akaneko.nsfw.maid());
-		message.channel.send(emebed);
+		akaneko.nsfw.maid().then((img) => {
+			const emebed = new Discord.MessageEmbed()
+				.setTitle(`${message.author.tag} here some maids (Maids, Maid Uniforms, etc, you know what maids are :3)`)
+				.setImage(img);
+			message.channel.send(emebed);
+
+		});
 	},
 };

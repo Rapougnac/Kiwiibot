8ea334b67614e0a0503ab1be769b789ce1d51561
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 12:10:43 +0100

    Add transfer command

---
+++ command_refactor/misc/economy/transfer.js
@@ -0,0 +1,22 @@
+const eco = require("discord-economy");
+
+module.exports = {
+	name: 'transfer',
+	aliases: [],
+	description: '',
+	category: 'Misc',
+	utilisation: '{prefix}transfer',
+	execute(client, message, args) {
+		const user = message.mentions.users.first()
+		const amount = args[1]
+
+		if (!user) return message.reply('Ping l\'utilisateur a qui tu veux donner de l\'argent !');
+		if (!amount) return message.reply('Spécifie le montant que tu vrux lui donner !');
+
+		const output = eco.FetchBalance(message.author.id);
+		if (output.balance < amount) return message.reply('Tu as moins de coins que le montant à transférer !');
+
+		const transfer = eco.Transfer(message.author.id, user.id, amount);
+		message.reply(`Transfert des coins réussi !\nPortefeuille de  ${message.author.tag}: ${transfer.FromUser}\nPortefeuille de ${user.tag}: ${transfer.ToUser}`);
+	},
+};

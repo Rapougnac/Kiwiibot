df66edfaae218fc295dd1090015fc1af7eb32215
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 17:42:17 +0100

    Add anime avatar command

---
+++ command_refactor/misc/neko/anime_avatar.js
@@ -0,0 +1,19 @@
+const nekoclient = require('nekos.life');
+const neko = new nekoclient();
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'anime avatar',
+	aliases: [],
+	description: '',
+	category: 'Misc',
+	utilisation: '{prefix}anime avatar',
+	execute(client, message, args) {
+		const GIF = await neko.sfw.avatar();
+		const embed = new Discord.MessageEmbed()
+			.setColor('#202225')
+			.setTitle(`${message.author.tag} here's a random anime avatar`)
+			.setImage(GIF.url)
+		message.channel.send(embed);
+	},
+};

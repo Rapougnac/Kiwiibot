bab1eb53a61e2bc2eb689c6ed8716902b4aac02f
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 17:18:39 +0100

    Add keta command

---
+++ command_refactor/misc/neko/keta.js
@@ -0,0 +1,19 @@
+const nekoclient = require('nekos.life');
+const neko = new nekoclient();
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'keta',
+	aliases: [],
+	description: '',
+	category: 'Misc',
+	utilisation: '{prefix}keta',
+	execute(client, message, args) {
+		const GIF = await neko.nsfw.keta();
+		const embed = new Discord.MessageEmbed()
+			.setColor('#202225')
+			.setTitle(`${message.author.tag} here's a random keta image/gif`)
+			.setImage(GIF.url)
+		message.channel.send(embed);
+	},
+};

7e731ac03680f4510468f671e37e4df9d9ae73be
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 17:46:50 +0100

    Add tickle command

---
+++ command_refactor/misc/neko/tickle.js
@@ -0,0 +1,29 @@
+const nekoclient = require('nekos.life');
+const neko = new nekoclient();
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'tickle',
+	aliases: [],
+	description: '',
+	category: 'Misc',
+	utilisation: '{prefix}tickle',
+	execute(client, message, args) {
+		if (message.mentions.members.size === 0) {
+			const GIF = await neko.sfw.tickle();
+			const embed = new Discord.MessageEmbed()
+				.setColor('#202225')
+				.setTitle(`${message.author.tag} tickled themsselves`)
+				.setImage(GIF.url)
+			message.channel.send(embed);
+		}
+		const member = message.mentions.members.first();
+		const GIF = await neko.sfw.tickle();
+		const embed = new Discord.MessageEmbed()
+			.setColor('#202225')
+			.setTitle(`${message.author.tag} tickled ${member.user.tag}`)
+			.setImage(GIF.url)
+		message.channel.send(embed);
+	},
+};
+

b804684e6e7a53cc72f368d8105d284e1cf7cfad
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 16:29:30 +0100

    Add tentacles command

---
+++ command_refactor/misc/akaneko/tentacles.js
@@ -0,0 +1,16 @@
+const akaneko = require('akaneko');
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'tentacles',
+	aliases: [],
+	description: '',
+	category: 'Misc',
+	utilisation: '{prefix}tentacles',
+	execute(client, message, args) {
+		const emebed = new Discord.MessageEmbed()
+			.setTitle(`${message.author.tag} here some tentacles (I don't judge you but...)`)
+			.setImage(await akaneko.nsfw.tentacles());
+		message.channel.send(emebed);
+	},
+};

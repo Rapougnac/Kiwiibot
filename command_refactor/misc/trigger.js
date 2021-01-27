797eb568fc66eb444ddead0a8e071cb9611efbdf
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 16:36:04 +0100

    Add trigger command

---
+++ command_refactor/misc/trigger.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const canva = require('canvacord');
+
+module.exports = {
+    name: 'join',
+    aliases: [],
+    description: '',
+    category: 'Misc',
+    utilisation: '{prefix}join',
+    execute(client, message, args) {
+        //const args = message.content.trim().split(/ +/g)
+        const avatar = message.author.displayAvatarURL({ dynamic: false, format: "png" });
+    
+        const image = await canva.Canvas.trigger(avatar);
+    
+        const triggered = new Discord.MessageAttachment(image, "triggered.gif");
+    
+        message.channel.send(triggered);
+    },
+};

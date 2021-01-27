3003391b315965f3cd2781ea3ce240d59860b824
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 21:03:37 +0100

    Add infobot command

--- index_refactor.js
+++ index_refactor.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const config = require('./config.json');
-const glob = require('glob')
+const glob = require('glob');
 const { Player } = require('discord-player');
 
 

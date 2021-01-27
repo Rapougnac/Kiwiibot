2c74bdd6411edfc1d83ac27e7413d5604f74ad3c
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 17:43:27 +0100

    Add cumsluts command

---
+++ command_refactor/misc/neko/cumsluts.js
@@ -0,0 +1,19 @@
+const nekoclient = require('nekos.life');
+const neko = new nekoclient();
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'cumsluts',
+	aliases: [],
+	description: '',
+	category: 'Misc',
+	utilisation: '{prefix}cumsluts',
+	execute(client, message, args) {
+		const GIF = await neko.nsfw.cumsluts();
+		const embed = new Discord.MessageEmbed()
+			.setColor('#202225')
+			.setTitle(`${message.author.tag} here's a random cumsluts image/gif`)
+			.setImage(GIF.url)
+		message.channel.send(embed);
+	},
+};

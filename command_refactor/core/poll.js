b152727142d9036bd9053ba75ec63bf1fc59b6f1
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 16:44:11 +0100

    Add poll command

---
+++ command_refactor/core/poll.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+module.exports = {
+    name: 'poll',
+    aliases: ['h'],
+    category: 'Core',
+    utilisation: '{prefix}poll <command name>',
+
+    execute(client, message, args) {
+        const reactions = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯', '🇰', '🇱', '🇲', '🇳', '🇴', '🇵', '🇶', '🇷', '🇸', '🇹', '🇺', '🇻']
+
+        //const args = message.content.trim().split(/ +/g);
+        const choices = args.join(' ').split(' | ').slice(1);
+        const question = args.slice(1).join(' ').split('|')[0];
+
+        if (!question) return message.channel.send('Veuillez indiquer la question à poser.');
+        if (!choices.length) return message.channel.send('Veuillez indiquer au moins 1 choix.');
+        if (choices.length > 20) return message.channel.send('Il ne peut pas y avoir plus de 20 choix.');
+        message.delete()
+        const sent = await message.channel.send(new Discord.MessageEmbed()
+            .setTitle(question)
+            .setDescription(choices.map((choice, i) => `${reactions[i]} ${choice}`).join('\n\n')));
+        for (i = 0; i < choices.length; i++)  sent.react(reactions[i]);
+    },
+};

df5700a25e9d2449bc45c205e5c7dd9d83d8eac1
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 17:45:58 +0100

    Add pat command

---
+++ command_refactor/misc/neko/pat.js
@@ -0,0 +1,29 @@
+const nekoclient = require('nekos.life');
+const neko = new nekoclient();
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'pat',
+	aliases: [],
+	description: '',
+	category: 'Misc',
+	utilisation: '{prefix}pat',
+	execute(client, message, args) {
+		if (message.mentions.members.size === 0) {
+			const GIF = await neko.sfw.pat();
+			const embed = new Discord.MessageEmbed()
+				.setColor('#202225')
+				.setTitle(`${message.author.tag} patted themsselves`)
+				.setImage(GIF.url)
+			message.channel.send(embed);
+		}
+		const member = message.mentions.members.first();
+		const GIF = await neko.sfw.pat();
+		const embed = new Discord.MessageEmbed()
+			.setColor('#202225')
+			.setTitle(`${message.author.tag} patted ${member.user.tag}`)
+			.setImage(GIF.url)
+		message.channel.send(embed);
+	},
+};
+

398db32aab2e3b69f56dbf1e3e4cccfba18c7c45
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 16:19:38 +0100

    Add gifs command

---
+++ command_refactor/misc/akaneko/gifs.js
@@ -0,0 +1,16 @@
+const akaneko = require('akaneko');
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'gifs',
+	aliases: [],
+	description: '',
+	category: 'Misc',
+	utilisation: '{prefix}gifs',
+	execute(client, message, args) {
+		const emebed = new Discord.MessageEmbed()
+			.setTitle(`${message.author.tag} here some gifs (Basically an animated image, so yes :3)`)
+			.setImage(await akaneko.nsfw.gifs());
+		message.channel.send(emebed);
+	},
+};

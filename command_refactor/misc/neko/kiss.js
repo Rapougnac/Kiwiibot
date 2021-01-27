883815fa1718c90727ce74ba995985a2d0c4c0a8
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 17:45:05 +0100

    Add kiss command

---
+++ command_refactor/misc/neko/kiss.js
@@ -0,0 +1,29 @@
+const nekoclient = require('nekos.life');
+const neko = new nekoclient();
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'kiss',
+	aliases: [],
+	description: '',
+	category: 'Misc',
+	utilisation: '{prefix}kiss',
+	execute(client, message, args) {
+		if (message.mentions.members.size === 0) {
+			const GIF = await neko.sfw.kiss();
+			const embed = new Discord.MessageEmbed()
+				.setColor('#202225')
+				.setTitle(`${message.author.tag} kissed themsselves`)
+				.setImage(GIF.url)
+			message.channel.send(embed);
+		}
+		const member = message.mentions.members.first();
+		const GIF = await neko.sfw.kiss();
+		const embed = new Discord.MessageEmbed()
+			.setColor('#202225')
+			.setTitle(`${message.author.tag} kissed ${member.user.tag}`)
+			.setImage(GIF.url)
+		message.channel.send(embed);
+	},
+};
+

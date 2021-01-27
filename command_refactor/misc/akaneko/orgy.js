5ce101461db52b1d7b17384f991ddb7ad01e1b4f
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 16:15:17 +0100

    Add orgy command

---
+++ command_refactor/misc/akaneko/orgy.js
@@ -0,0 +1,16 @@
+const akaneko = require('akaneko');
+const Discord = require("discord.js");
+
+module.exports = {
+    name: 'orgy',
+    aliases: [],
+	description: '',
+	category: 'Misc',
+	utilisation: '{prefix}orgy',
+	execute(client,message,args) {
+		const emebed = new Discord.MessageEmbed()
+		.setTitle(`${message.author.tag} here some orgy (An orgy of food sure :))`)
+		.setImage(await akaneko.nsfw.orgy());
+	  message.channel.send(emebed);
+	},
+};

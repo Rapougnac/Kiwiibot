baa1e33f1aca6ca5bb4fb84012175bb9110b9d0c
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 17:46:25 +0100

    Add smug command

---
+++ command_refactor/misc/neko/smug.js
@@ -0,0 +1,19 @@
+const nekoclient = require('nekos.life');
+const neko = new nekoclient();
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'smug',
+	aliases: [],
+	description: '',
+	category: 'Misc',
+	utilisation: '{prefix}smug',
+	execute(client, message, args) {
+		const GIF = await neko.sfw.smug();
+		const embed = new Discord.MessageEmbed()
+			.setColor('#202225')
+			.setTitle(`${message.author.tag} here's a random smug image/gif`)
+			.setImage(GIF.url)
+		message.channel.send(embed);
+	},
+};

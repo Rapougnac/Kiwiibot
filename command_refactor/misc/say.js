125e1db9e5005f0b5eb1a5197eba2ca22c90cf79
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 16:33:37 +0100

    Add say command

---
+++ command_refactor/misc/say.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: 'say',
+    aliases: [],
+    description: '',
+    category: 'Misc',
+    utilisation: '{prefix}say',
+    execute(client, message, args) {
+        //const args = message.content.trim().substring(prefix.length).split(/ +/g);
+        const botmessage = args.join(" ").slice(0).slice(1).slice(2);
+        message.delete().catch();
+        message.channel.send(botmessage);
+    },
+};

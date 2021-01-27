ce098efd7178c3bcf1bc2f0ece6317d8571d3f33
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 16:28:50 +0100

    Add tighs command

---
+++ command_refactor/misc/akaneko/tighs.js
@@ -0,0 +1,16 @@
+const akaneko = require('akaneko');
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'tighs',
+	aliases: [],
+	description: '',
+	category: 'Misc',
+	utilisation: '{prefix}tighs',
+	execute(client, message, args) {
+		const emebed = new Discord.MessageEmbed()
+			.setTitle(`${message.author.tag} here some tighs (this is great)`)
+			.setImage(await akaneko.nsfw.thighs());
+		message.channel.send(emebed);
+	},
+};

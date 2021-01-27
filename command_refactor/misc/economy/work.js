28f0ea61e3e3ca8227e54ee66a3fe21aaf6ebb03
A2va <49582555+A2va@users.noreply.github.com>
2021-01-27 16:13:29 +0100

    Add slots command

--- command_refactor/misc/economy/work.js
+++ command_refactor/misc/economy/work.js
@@ -11,13 +11,13 @@ module.exports = {
 			failurerate: 30,
 			money: Math.floor(Math.random() * 500),
 			jobs: ['caissier(ière)', 'commerçant(e)', 'strip-teaser(euse)', 'manager', 'barman', 'pornstar']
-		})
+		});
 		//10% chance to fail and earn nothing. You earn between 1-500 coins. And you get one of those 3 random jobs.
-		if (output.earned == 0) return message.reply('Ah, malheuruesement tu as mal fait ton travail tu n\'as rien gagné !')
+		if (output.earned == 0) return message.reply('Ah, malheuruesement tu as mal fait ton travail tu n\'as rien gagné !');
 
 		message.channel.send(`${message.author.username}
 	  Tu as travaillé en tant que \` ${output.job} \` et tu as gagné :money_with_wings: ${output.earned}
-	  Tu as maintenant :money_with_wings: ${output.balance}`)
+	  Tu as maintenant :money_with_wings: ${output.balance}`);
 
 	},
 };

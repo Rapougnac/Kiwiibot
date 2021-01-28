2c1a5e4309fd9286dad9b4f6b67b1c2b807a8286
A2va <49582555+A2va@users.noreply.github.com>
2021-01-28 18:16:52 +0100

    Fix async function for amethyste

--- commands/misc/amethyste/blurple.js
+++ commands/misc/amethyste/blurple.js
@@ -8,13 +8,14 @@ module.exports = {
     description: '',
     category: 'Misc',
     utilisation: '{prefix}blurple',
-    execute(client, message, args) {
+    async execute(client, message, args) {
         const AmeAPI = new AmeClient(config.amethyste.client); {
 
             //const args = message.content.trim().split(/ +/g);
-            const User =  message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase().includes() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase().includes() === args.join(' ').toLocaleLowerCase())
-              || message.member; let m =  message.channel.send("**Please Wait...**");
-            const buffer =  AmeAPI.generate("blurple", { url: User.user.displayAvatarURL({ format: "png", size: 2048 }) });
+            const User = await  message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase().includes() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase().includes() === args.join(' ').toLocaleLowerCase())
+              || message.member; 
+            let m = await message.channel.send("**Please Wait...**");
+            const buffer = await AmeAPI.generate("blurple", { url: User.user.displayAvatarURL({ format: "png", size: 2048 }) });
             const attachment = new Discord.MessageAttachment(buffer, "blurple.png");
             m.delete({ timeout: 5000 });
             message.channel.send(attachment);

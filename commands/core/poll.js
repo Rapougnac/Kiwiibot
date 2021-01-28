a70e8cd8558a985eb5565c35d2da1ed2beddea58
A2va <49582555+A2va@users.noreply.github.com>
2021-01-28 20:51:47 +0100

    Fixing poll command

--- commands/core/poll.js
+++ commands/core/poll.js
@@ -10,13 +10,13 @@ module.exports = {
 
         //const args = message.content.trim().split(/ +/g);
         const choices = args.join(' ').split(' | ').slice(1);
-        const question = args.slice(1).join(' ').split('|')[0];
+        const question = args.join(' ').split('|')[0];
 
         if (!question) return message.channel.send('Veuillez indiquer la question à poser.');
         if (!choices.length) return message.channel.send('Veuillez indiquer au moins 1 choix.');
         if (choices.length > 20) return message.channel.send('Il ne peut pas y avoir plus de 20 choix.');
         message.delete()
-        const sent = message.channel.send(new Discord.MessageEmbed()
+        const sent = await message.channel.send(new Discord.MessageEmbed()
             .setTitle(question)
             .setDescription(choices.map((choice, i) => `${reactions[i]} ${choice}`).join('\n\n')));
         for (i = 0; i < choices.length; i++)  sent.react(reactions[i]);

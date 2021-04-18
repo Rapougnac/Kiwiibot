e2104d3ab2200a708e6d26bfc19054a0838745ac
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-04-18 21:09:24 +0200

    Update ping.js

--- src/commands/infos/ping.js
+++ src/commands/infos/ping.js
@@ -5,6 +5,8 @@ function formatNumber(number, minimumFractionDigits = 0) {
 		maximumFractionDigits: 2
 	});
 }
+const { Message, MessageEmbed } = require("discord.js");
+const Client = require("../../struct/Client")
 
 module.exports = {
     name: 'ping',
@@ -17,14 +19,22 @@ module.exports = {
 	ownerOnly: false,
 	guildOnly: false,
 	adminOnly: false,
-	permissions: ["SEND_MESSAGES"],
+	permissions: ["SEND_MESSAGES", "VIEW_CHANNEL"],
 	clientPermissions: [],
 	string: [],
+	/**
+	 * 
+	 * @param {Client} client 
+	 * @param {Message} message 
+	 * @param {String[]} args 
+	 */
 	async execute(client,message,args) {
+        message.channel.startTyping();
 		const msg = await message.channel.send(`🏓 Pinging....`);
 		const ping = /*Math.round*/(msg.createdTimestamp - message.createdTimestamp);
 		const string = this.string[0].format(ping, formatNumber((client.ws.ping)));
         //editing it to the actual latency
         msg.edit(stripIndents`${string}`);
+		message.channel.stopTyping();
 	},
 };

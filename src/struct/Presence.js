0fd709e61d759895dad316b72f037c004995d5f8
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-04-25 13:06:04 +0200

    Delete Presence.js

--- src/struct/Presence.js
+++
@@ -1,8 +0,0 @@
-const { Presence } = require("discord.js");
-module.exports = class ExtendedPresence extends Presence {
-    constructor(settings) {
-        super(settings.data || {});
-
-        this.presence = (x);
-    }
-};

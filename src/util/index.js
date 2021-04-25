eaef845a6d02a5a7c0623f0a8125a23c1800d0e3
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-04-25 13:08:14 +0200

    Update index.js

--- src/util/index.js
+++ src/util/index.js
@@ -1,34 +1,35 @@
 const { readdirSync } = require('fs');
 
-function getCommands () {
-    let categories = [];
+function getCommands() {
+  let categories = [];
 
-    readdirSync('./commands/').forEach((dir) => {
-        const directories = readdirSync(`./commands/${dir}`).filter((file) => file.endsWith('.js'));
-        const value = [];
-        const commands = directories.map((command) => {
-            const file = require(`../commands/${dir}/${command}`)
+  readdirSync('./commands/').forEach((dir) => {
+    const directories = readdirSync(`./commands/${dir}`).filter((file) =>
+      file.endsWith('.js')
+    );
+    const value = [];
+    const commands = directories.map((command) => {
+      const file = require(`../commands/${dir}/${command}`);
 
-            value.push({
-                name: file.name ? file.name : 'No command name',
-                description: file.description ? file.description : 'No description',
-                aliases: file.aliases ? file.aliases : 'No aliases',
-                category: file.category ? file.category : 'No category',
-                utilisation: file.utilisation ? file.utilisation : 'No utilisation',
-            });
-        });
-
-        let data = new Object();
-
-        data = {
-            name: dir.toUpperCase(),
-            value,
-        }
-
-        categories.push(data);
-        
+      value.push({
+        name: file.name ? file.name : 'No command name',
+        description: file.description ? file.description : 'No description',
+        aliases: file.aliases ? file.aliases : 'No aliases',
+        category: file.category ? file.category : 'No category',
+        utilisation: file.utilisation ? file.utilisation : 'No utilisation',
+      });
     });
-    return categories;
-};
 
-module.exports = { getCommands }
+    let data = new Object();
+
+    data = {
+      name: dir.toUpperCase(),
+      value,
+    };
+
+    categories.push(data);
+  });
+  return categories;
+}
+
+module.exports = { getCommands };

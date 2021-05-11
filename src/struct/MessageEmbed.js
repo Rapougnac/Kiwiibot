a0f4ebafd311c616439db96edc9c7550c245d962
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-05-11 22:23:54 +0200

    Update MessageEmbed.js

--- src/struct/MessageEmbed.js
+++ src/struct/MessageEmbed.js
@@ -6,7 +6,6 @@ const { MessageEmbed } = require("discord.js");
  */
 module.exports = class ExtendedMessageEmbed extends MessageEmbed {
     /**
-     * 
      * @param {Boolean} [inline] The inline of the embed, leave it empty will return `false`
      * @returns 
      */
@@ -16,5 +15,17 @@ module.exports = class ExtendedMessageEmbed extends MessageEmbed {
         if(typeof inline !== "boolean") throw new TypeError("The inline cannot be other than a boolean");
         return super.addField("\u200b", "\u200b", inline);
     }
+    setTitle(title) {
+        title = resolveString(title);
+        if (title.length > 256) throw new RangeError('RichEmbed titles may not exceed 256 characters.');
+        this.title = title;
+        return this;
+    }
 
 };
+
+const resolveString = (data) =>  {
+    if(typeof data === 'string') return data;
+    if(data instanceof Array) return data.join('\n');
+    return String(data);
+}

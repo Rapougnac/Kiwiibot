fd0cd5903052488b11b517fe1d684c6576c52caf
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-05-26 17:47:19 +0200

    Update prefix-reset.js

--- src/commands/core/prefix-reset.js
+++ src/commands/core/prefix-reset.js
@@ -1,38 +1,43 @@
-const prefixSchema = require('../../models/PrefixSchema')
-const { confirmation } = require('@reconlx/discord.js');
-const { Message, Client } = require("discord.js");
-module.exports = {
-    name : 'prefix-reset',
-    aliases: ["pr", "clearprefix", "resetprefix"],
-    category: "Core",
-    utilisation: "{prefix}prefix-reset",
-    description: "Reset the prefix",
-    cooldown: 5,
-    nsfw: false,
-    guildOnly: false,
-    ownerOnly: false,
-    adminOnly: true,
-    permissions: [],
-    clientPermissions: ["VIEW_CHANNEL", "SEND_MESSAGES"],
-    string: [],
-    /**
-     * @param {Client} client 
-     * @param {Message} message 
-     * @param {String[]} args 
-     */
-    async execute(client, message, args) {
-        message.channel.send(this.string[0]).then(async (msg) => {
-            const emoji = await confirmation(msg, message.author, ['✅', '❌'], 10000)
-            if(emoji === '✅') {
-                msg.delete()
-                await prefixSchema.findOneAndDelete({ GuildID: message.guild.id })
-                message.channel.send(this.string[1].format(client.prefix))
-            }
-            if(emoji === '❌') {
-                msg.delete()
-                return message.channel.send(this.string[2])
-            }
-        })
+const prefixSchema = require('../../models/PrefixSchema');
+const { confirmation } = require('../../util/confirmation');
+const { Message, Client } = require('discord.js');
 
-    },
+module.exports = {
+  name: 'prefix-reset',
+  aliases: ['pr', 'clearprefix', 'resetprefix'],
+  category: 'Core',
+  utilisation: '{prefix}prefix-reset',
+  description: 'Reset the prefix',
+  cooldown: 5,
+  nsfw: false,
+  guildOnly: false,
+  ownerOnly: false,
+  adminOnly: true,
+  permissions: [],
+  clientPermissions: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
+  string: [],
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  async execute(client, message, args) {
+    message.channel.send(this.string[0]).then(async (msg) => {
+      const emoji = await confirmation(
+        msg,
+        message.author,
+        ['✅', '❌'],
+        10000
+      );
+      if (emoji === '✅') {
+        msg.delete();
+        await prefixSchema.findOneAndDelete({ GuildID: message.guild.id });
+        message.channel.send(this.string[1].format(client.prefix));
+      }
+      if (emoji === '❌') {
+        msg.delete();
+        return message.channel.send(this.string[2]);
+      }
+    });
+  },
 };

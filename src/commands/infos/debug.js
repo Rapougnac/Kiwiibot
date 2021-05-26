9a4c86985d468c22d61abca35bb6dc48bc727888
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-05-26 17:44:34 +0200

    Reworked debug.js

--- src/commands/infos/debug.js
+++ src/commands/infos/debug.js
@@ -1,5 +1,6 @@
-const Embed = require("@struct/MessageEmbed");
-const { version } = require('../../../package.json')
+const { MessageEmbed: Embed, Message, version: discordjs_version, MessageEmbed } = require('discord.js');
+const Client = require('../../struct/Client')
+const { version } = require('../../../package.json');
 const moment = require('moment');
 require("moment-duration-format");
 
@@ -8,18 +9,29 @@ module.exports = {
     aliases: [],
     category: 'Infos',
     utilisation: '{prefix}debug',
-
-    async execute(client, message,args) {
+    cooldown: 20,
+    nsfw: false,
+    guildOnly: false,
+    adminOnly: false,
+    ownerOnly: false,
+    permissions: [],
+    clientPermissions: ['SEND_MESSAGES', 'VIEW_CHANNEL', 'EMBED_LINKS'],
+    /**
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    async execute(client, message, args) {
         const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
         message.channel.send(new Embed({
             color: 'BLUE',
             title: 'Debug infos:',
-            description: 'Infos about the bot',
             fields: [
                 { name: 'Bot\'s name and tag', value: client.user.tag },
                 { name: 'Version', value:  version },
-                { name: 'Server command prefix', value: 'm?' },
-                { name: 'Time since last restart', value: `${duration}` },
+                { name: 'Server command prefix', value: client.prefix },
+                { name: 'Time since last restart', value: duration },
+                { name: 'Discord.js Version', value: discordjs_version },
                 
             ]
         }))

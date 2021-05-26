888ce82411431f6913a1f5dea4db31552a84c905
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-05-26 17:51:25 +0200

    Update guildDelete.js

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,19 +1,19 @@
-// const PrefixSchema = require('../models/PrefixSchema');
-// const Client = require('../struct/Client');
-// const { Guild } = require('discord.js');
+const PrefixSchema = require('../models/PrefixSchema');
+const Client = require('../struct/Client');
+const { Guild } = require('discord.js');
 
-// /**
-//  *
-//  * @param {Client} client
-//  * @param {Guild} guild
-//  */
-// module.exports = async (client, guild) => {
-//   PrefixSchema.findOne({ GuildID: guild.id }, async (err, data) => {
-//     if (err) throw err;
-//     if (data) {
-//       PrefixSchema.findOneAndDelete({ GuildID: guild.id }).then(
-//         console.log('deleted data.')
-//       );
-//     }
-//   });
-// };
+/**
+ * @param {Client} client
+ * @param {Guild} guild
+ * @returns {Promise<void>}
+ */
+module.exports = async (client, guild) => {
+  PrefixSchema.findOne({ GuildID: guild.id }, async (err, data) => {
+    if (err) throw err;
+    if (data) {
+      PrefixSchema.findOneAndDelete({ GuildID: guild.id }).then(
+        console.log('deleted data.')
+      );
+    }
+  });
+};

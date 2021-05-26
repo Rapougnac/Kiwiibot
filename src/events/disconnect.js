cfc0e86b922895feef9850ed3e635660b409465c
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-05-26 17:44:07 +0200

    Deleted unused events

--- src/events/disconnect.js
+++
@@ -1,9 +0,0 @@
-//here the event starts
-const { Client } = require("discord.js");
-/**
- * 
- * @param {Client} client 
- */
-module.exports = (client) => {
-    console.log(`${client.user.tag} has been disconnected at ${new Date()}.`)
-}

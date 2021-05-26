a4984fb59cf6b7bac741f752f1ef3b65ecd8393c
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-05-26 17:46:17 +0200

    Update docs.js

--- src/commands/core/docs.js
+++ src/commands/core/docs.js
@@ -2,26 +2,49 @@ const { Client, Message, MessageEmbed } = require('discord.js');
 const axios = require('axios');
 
 module.exports = {
-  name: 'djs',
-  aliases: ['docs'],
+  name: 'docs',
+  aliases: ['doc', 'documentation'],
+  description: 'Get the djs doc in an embed, you can spefify the source by doing `--src {source}` (without the brackets) The sources are listed here: `stable`, `master`, `commando`, `rpc`, `akairo`, `akairo-master` and `collection`',
   category: 'Core',
-
+  utilisation: '{prefix}docs [query] <--src> <[source]>',
+  cooldown: 5,
+  nsfw: false,
+  guildOnly: false,
+  adminOnly: false,
+  ownerOnly: false,
+  permissions: [],
+  clientPermissions: ['SEND_MESSAGES', 'VIEW_CHANNEL', 'EMBED_LINKS'],
+  string: [],
   /**
    * @param {Client} client
    * @param {Message} message
    * @param {String[]} args
    */
   async execute(client, message, args) {
-    const query = args.join(' ');
-    if (!query) return message.reply(`Please specify a query!`);
-    const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(
-      query
-    )}`;
+    const query = args[0];
+    if (!query) return message.reply(this.string[0]);
+    let source;
+    if(message.content.includes('--src')) {
+      source = args[2];
+      const url = `https://djsdocs.sorta.moe/v2/embed?src=${source}&q=${encodeURIComponent(
+        query
+      )}`;
+      axios.get(url).then(({ data }) => {
+        if (data) {
+          message.channel.send({ embed: data });
+        }
+      });
+    } else { 
+      source = 'stable';
+      const url = `https://djsdocs.sorta.moe/v2/embed?src=${source}&q=${encodeURIComponent(
+        query
+      )}`;
+      axios.get(url).then(({ data }) => {
+        if (data) {
+          message.channel.send({ embed: data });
+        }
+      });
+    }
 
-    axios.get(url).then(({ data }) => {
-      if (data) {
-        message.channel.send({ embed: data });
-      }
-    });
   },
 };

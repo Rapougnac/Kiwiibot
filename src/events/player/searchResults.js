2823a097c72efd9d7021729e3282a79051a53b9c
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-05-26 17:52:04 +0200

    Update searchResults.js

--- src/events/player/searchResults.js
+++ src/events/player/searchResults.js
@@ -1,3 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+const { Track } = require('discord-player')
+const Client = require('../../struct/Client')
+const { Message } = require('discord.js')
+/**
+ * 
+ * @param {Client} client 
+ * @param {Message} message 
+ * @param {String} query 
+ * @param {Track} tracks 
+ */
 module.exports = (client, message, query, tracks) => {
     message.channel.send({
         embed: {

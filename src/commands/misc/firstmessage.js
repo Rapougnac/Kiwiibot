682f48953e08696905e0fbb4141bf87a1f768ae4
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-05-27 18:35:43 +0200

    Update firstmessage.js

--- src/commands/misc/firstmessage.js
+++ src/commands/misc/firstmessage.js
@@ -1,22 +1,36 @@
-const Discord = require("discord.js");
+const  { MessageEmbed, Message } = require("discord.js");
+const Client = require('../../struct/Client');
 module.exports = {
     name: 'firstmessage',
     aliases: ['fm'],
-    description: '',
+    description: 'Get the first message in the current channel',
     category: 'Misc',
     utilisation: '{prefix}firstmessage',
+    cooldown: 10,
+    nsfw: false,
+    guildOnly: false,
+    adminOnly: false,
+    ownerOnly: false,
+    permissions: [],
+    clientPermissions: ['SEND_MESSAGES', 'VIEW_CHANNEL', 'EMBED_LINKS'],
+    string: [],
+    /**
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String} args 
+     */
     async execute(client, message, args) {
         const fetchMessages = await message.channel.messages.fetch({ after: 1, limit: 1 });
         const msg = fetchMessages.first();
     
-        const embed = new Discord.MessageEmbed()
-          .setTitle(`First message in ${message.channel.name}`)
+        const embed = new MessageEmbed()
+          .setTitle(this.string[0].format(message.channel.name))
           .setURL(msg.url)
           .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
-          .setDescription(`Content ` + msg.content)
-          .addField(`Author`, msg.author, true)
-          .addField(`Message ID:`, msg.id, true)
-          .addField(`Created at:`, msg.createdAt.toLocaleDateString(), true)
+          .setDescription(this.string[1] + msg.content)
+          .addField(this.string[2], msg.author, true)
+          .addField(this.string[3], msg.id, true)
+          .addField(this.string[4], msg.createdAt.toLocaleDateString(), true)
         message.channel.send(embed)
     },
 };

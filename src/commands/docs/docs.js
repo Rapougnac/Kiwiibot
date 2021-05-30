0934200d9370156e38fb60f5272411c7f9fd47ee
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-05-30 12:39:13 +0200

    Moved docs.js

--- src/commands/docs/docs.js
+++ src/commands/docs/docs.js
@@ -4,7 +4,8 @@ const axios = require('axios');
 module.exports = {
   name: 'docs',
   aliases: ['doc', 'documentation'],
-  description: 'Get the djs doc in an embed, you can spefify the source by doing `--src {source}` (without the brackets) The sources are listed here: `stable`, `master`, `commando`, `rpc`, `akairo`, `akairo-master` and `collection`',
+  description:
+    'Get the djs doc in an embed, you can spefify the source by doing `--src {source}` (without the brackets) The sources are listed here: `stable`, `master`, `commando`, `rpc`, `akairo`, `akairo-master` and `collection`',
   category: 'Core',
   utilisation: '{prefix}docs [query] <--src> <[source]>',
   cooldown: 5,
@@ -21,20 +22,22 @@ module.exports = {
    * @param {String[]} args
    */
   async execute(client, message, args) {
-    const query = args[0];
+    const query = args.join(' ').split(/ +--src/g)[0];
     if (!query) return message.reply(this.string[0]);
     let source;
-    if(message.content.includes('--src')) {
-      source = args[2];
+    if (message.content.includes('--src')) {
+      source = args[args.length - 1];
       const url = `https://djsdocs.sorta.moe/v2/embed?src=${source}&q=${encodeURIComponent(
         query
       )}`;
       axios.get(url).then(({ data }) => {
         if (data) {
           message.channel.send({ embed: data });
+        } else {
+          return message.channel.send(this.string[1]);
         }
       });
-    } else { 
+    } else {
       source = 'stable';
       const url = `https://djsdocs.sorta.moe/v2/embed?src=${source}&q=${encodeURIComponent(
         query
@@ -42,9 +45,10 @@ module.exports = {
       axios.get(url).then(({ data }) => {
         if (data) {
           message.channel.send({ embed: data });
+        } else {
+          return message.channel.send(this.string[1]);
         }
       });
     }
-
   },
 };

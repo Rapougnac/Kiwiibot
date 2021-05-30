d1438d0c3d7094a0c0b7639471333f9b4d3b5f6b
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-05-30 12:38:30 +0200

    Added slash command

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { APIMessage, MessageEmbed } = require('discord.js');
+const axios = require('axios');
 const Client = require('./src/struct/Client'); // Extended Client
 require('./src/struct/Message'); // Extended Message
 require('module-alias/register'); // Module alias for the path
@@ -23,3 +25,90 @@ client.listentoProcessEvents(['uncaughtException', 'unhandledRejection'], {
   logsonboth: true,
 });
 
+client.ws.on('INTERACTION_CREATE', async (interaction) => {
+  console.log(interaction);
+  const { member } = interaction;
+  const { options, name } = interaction.data;
+  const command = name.toLowerCase();
+
+  const args = {};
+  this.args = args;
+
+  if (options) {
+    for (const option of options) {
+      const { name, value } = option;
+      args[name] = value;
+    }
+  }
+
+  console.log(args);
+  switch (command) {
+    case 'ping': {
+      reply(
+        interaction,
+        `🏓 P${'o'.repeat(
+          Math.min(Math.round(client.ws.ping / 100), 1500)
+        )}ng \n\`${client.ws.ping}ms\``
+      );
+      break;
+    }
+
+    case 'docs': {
+      const query = args?.query;
+      let source = args?.source;
+      const url = `https://djsdocs.sorta.moe/v2/embed?src=${source}&q=${encodeURIComponent(
+        query
+      )}`;
+      if (!source) source = 'stable';
+      axios.get(url).then(({ data }) => {
+        const embed = new MessageEmbed(data);
+        if (data) {
+          reply(interaction, embed);
+        }
+      });
+      break;
+    }
+
+    case 'avatar': {
+      let user = args?.user;
+      if (!user) user = member.user.id;
+      const u = (await client.users.fetch(user)).displayAvatarURL({
+        size: 4096,
+        dynamic: true,
+        format: 'png',
+      });
+      reply(interaction, u);
+      break;
+    }
+  }
+});
+
+const reply = async (interaction, response) => {
+  let data = {
+    content: response,
+  };
+
+  if (typeof response === 'object') {
+    data = await createAPIMessage(interaction, response);
+  } 
+  if(typeof response === 'object' && this.args?.target){
+    data = await createAPIMessage(interaction, response, `Heyy <@!${this.args?.target}> voilà quelque chose qui pourrait t'être utile`)
+  }
+  client.api.interactions(interaction.id, interaction.token).callback.post({
+    data: {
+      type: 4,
+      data,
+    },
+  });
+};
+
+const createAPIMessage = async (interaction, content, str) => {
+  const { data, files } = await APIMessage.create(
+    client.channels.resolve(interaction.channel_id),
+    str, content
+  )
+    .resolveData()
+    .resolveFiles();
+
+  return { ...data, files };
+};

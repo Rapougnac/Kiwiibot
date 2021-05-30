795e9a3b2b3fbbe5615eaceab78983cae25336f8
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-05-30 12:40:41 +0200

    Create sendpins.js

---
+++ src/commands/docs/sendpins.js
@@ -0,0 +1,31 @@
+const { Message, MessageEmbed, MessageAttachment } = require('discord.js');
+const Client = require('../../struct/Client');
+const moment = require('moment');
+module.exports = {
+    name: 'sendpins',
+    aliases: ['sendpinned'],
+    description: '',
+    category: '',
+    utilisation: '{prefix}',
+    cooldown: 5,
+    nsfw: false,
+    ownerOnly: false,
+    adminOnly: false,
+    guildOnly: false,
+    permissions: [],
+    clientPermissions: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    async execute(client, message, args) {
+        const pinned = (await message.channel.messages.fetchPinned()).array();
+        pinned.reverse();
+        let str = String();
+        pinned.forEach((pin, count) => {
+            str += `**${count +1}** - ${pin.url} - Auteur: ${pin.author.username}\n`;
+        });
+        message.channel.send(str);
+    },  
+};

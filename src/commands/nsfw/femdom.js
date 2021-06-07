c57d6628b27585a42c7a24893aed0d9acb50c7ed
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-06-07 10:17:16 +0200

    Update femdom.js

--- src/commands/nsfw/femdom.js
+++ src/commands/nsfw/femdom.js
@@ -1,26 +1,26 @@
+const Command = require('../../struct/Command');
 const nekoclient = require('nekos.life');
 const neko = new nekoclient();
-const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+module.exports = class FemDomCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'femdom',
+      aliases: [],
+      description: 'Get a pic of femdom hentai',
+      category: 'infos',
+      cooldown: 120,
+      utilisation: '{prefix}ping',
+      nsfw: true,
+    });
+  }
 
-module.exports = {
-  name: 'femdom',
-  aliases: [],
-  description: '',
-  category: 'nsfw',
-  utilisation: '{prefix}femdom',
-  async execute(client, message, args) {
-    if (message.channel.nsfw) {
-      const GIF = await neko.nsfw.femdom();
-      const embed = new Discord.MessageEmbed()
+  async execute(client, message) {
+     const GIF = await neko.nsfw.femdom();
+      const embed = new MessageEmbed()
         .setColor('#202225')
-        .setTitle(`${message.author.tag} here's a random femdom image/gif`)
+        .setAuthor(`${message.author.tag} here's a random femdom image/gif`, message.author.displayAvatarURL({ dynamic: true, size: 512, format: 'png' }))
         .setImage(GIF.url);
       message.channel.send(embed);
-    } else {
-      let m = await message.channel.send(
-        '**Warning** this command cannot be used in non-nsfw channels!'
-      );
-      m.delete({ timeout: 10000 });
-    }
-  },
-};
+  }
+}

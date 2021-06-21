d88799fde16c50080a244ab537814156e9a3bf06
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-06-21 21:38:46 +0200

    Update string.js

--- src/util/string.js
+++ src/util/string.js
@@ -1,4 +1,48 @@
-const list = new Intl.ListFormat('en');
+// eslint-disable-next-line no-unused-vars
+const { Message, UserFlags, BitField } = require('discord.js');
+const languageSchema = require('../models/languageSchema');
+let list;
+/**
+ * Check the lang of the list
+ * @param {Message} message
+ */
+const checkLang = async (message) => {
+  let lang;
+  if (message.guild) {
+    try {
+      await languageSchema.findOne(
+        {
+          _id: message.guild.id,
+        },
+        (err, data) => {
+          if (err) throw err;
+          if (!data)
+            data = new languageSchema({
+              _id: message.guild.id,
+              language: 'english',
+            });
+          lang = data.language;
+        }
+      );
+      switch (lang) {
+        case 'english': {
+          lang = 'en';
+          break;
+        }
+        case 'french': {
+          lang = 'fr';
+          break;
+        }
+      }
+    } catch (e) {
+      console.error(e);
+    }
+    list = new Intl.ListFormat(lang);
+  } else {
+    lang = 'en';
+    list = new Intl.ListFormat(lang);
+  }
+};
 // All functions returned from this module will now be in string format
 
 /**
@@ -8,8 +52,11 @@ const list = new Intl.ListFormat('en');
  * @param {string} end the end of the string indicating it's truncated
  * @returns {string} Truncated string
  */
-function textTruncate(str = '', length = 100, end = '...'){
-  return String(str).substring(0, length - end.length) + (str.length > length ? end : '');
+function textTruncate(str = '', length = 100, end = '...') {
+  return (
+    String(str).substring(0, length - end.length) +
+    (str.length > length ? end : '')
+  );
 }
 
 /**
@@ -19,23 +66,22 @@ function textTruncate(str = '', length = 100, end = '...'){
  * @returns {string} Ordinalized number
  * @note Does not support negative numbers!
  */
-function ordinalize(n = 0){
-  return Number(n)+['st','nd','rd'][n/10%10^1&&n%10]||Number(n)+'th';
+function ordinalize(n = 0) {
+  return (
+    Number(n) + ['st', 'nd', 'rd'][(n / 10) % 10 ^ 1 && n % 10] ||
+    Number(n) + 'th'
+  );
 }
 
 /**
- * Converts number to string and adds a comma separator
- * @param {number|string} number the Number to convert
- * @param {number} maximumFractionDigits the number of decimal places to include in result
- * @example commatize(11235) will return `11,235`; commatize(1234.567, 2) will return `1,234.57`
- * @returns {string} number with commas
- * @note Numbers greater than 10e307 will return '∞'
+ * 
+ * @param {number|string} number The number to separte
+ * @example separateNumbers(123456) will return `123'456`; separateNumbers(1234.567) will return `1'234.567`;
+ * @returns {string} The numbers with quotation marks
  */
-function commatize(number, maximumFractionDigits = 2){
-  return Number(number || '')
-  .toLocaleString('en-US', { maximumFractionDigits });
+function separateNumbers(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "'");
 }
-
 /**
  * Converts a number to a stringified compact version
  * @param {number|string} number the Number to convert
@@ -44,10 +90,10 @@ function commatize(number, maximumFractionDigits = 2){
  * @returns {string} compact version of the number
  * @note Maximum number for optimal usage is 10e13
  */
-function compactNum(number, maximumFractionDigits = 2){
-  return Number(number || '')
-  .toLocaleString('en-US', {
-    notation: 'compact', maximumFractionDigits
+function compactNum(number, maximumFractionDigits = 2) {
+  return Number(number || '').toLocaleString('en-US', {
+    notation: 'compact',
+    maximumFractionDigits,
   });
 }
 
@@ -56,13 +102,13 @@ function compactNum(number, maximumFractionDigits = 2){
  * @param {array} array the array to join
  * @returns {string} the joined array
  */
-function joinArray(array = []){
-  return list.format(array.map(x => String(x)));
+function joinArray(array = []) {
+  return list.format(array.map((x) => String(x)));
 }
 
 /**
  * Join array and add a limiter.
- * @param {array} array the array to join
+ * @param {Array} array the array to join
  * @param {number} limit the maximum length of the string output
  * @param {string} connector similar to param of `array.join()`
  * @example joinArrayAndLimit([1,2,3,4,5,6,7,8,9,10,11]) will return  text: '1, 2, 3, 4', excess: 6
@@ -70,11 +116,14 @@ function joinArray(array = []){
  * @returns {object.excess} The number of elements not included on join
  * @note Will throw a typeerror array.reduce is not a function if param1 is not of type array.
  */
-function joinArrayAndLimit(array = [], limit = 1000, connector = '\n'){
-  return array.reduce((a,c,i,_x) => a.text.length + String(c).length > limit
-  ? { text: a.text, excess: a.excess + 1 }
-  : { text: a.text + (i ? connector : '') + String(c), excess: a.excess }
-  , { text: '', excess: 0});
+function joinArrayAndLimit(array = [], limit = 1000, connector = '\n') {
+  return array.reduce(
+    (a, c, i) =>
+      a.text.length + String(c).length > limit
+        ? { text: a.text, excess: a.excess + 1 }
+        : { text: a.text + (i ? connector : '') + String(c), excess: a.excess },
+    { text: '', excess: 0 }
+  );
 }
 
 /**
@@ -82,16 +131,40 @@ function joinArrayAndLimit(array = [], limit = 1000, connector = '\n'){
  * @param {string} text The string to clean
  * @returns {string} the cleaned string
  */
-function clean(text){
-  return String(text).replace(/`/g, `\`${String.fromCharCode(8203)}`).replace(/@/g, `@${String.fromCharCode(8203)}`)
+function clean(text) {
+  return String(text)
+    .replace(/`/g, `\`${String.fromCharCode(8203)}`)
+    .replace(/@/g, `@${String.fromCharCode(8203)}`);
 }
-
+/**
+ * Convert flags bitfield to string in array
+ * @param {BitField} bitfield The bitfield to pass in
+ * @returns {String[]} The array of the user flags
+ */
+function convertUFB(bitfield) {
+  if (!bitfield) throw 'Missing Bitfield';
+  if (isNaN(bitfield)) throw `${bitfield} is not a number`;
+  let processConvert = bitfield;
+  let UFConvertResult = [];
+  const ACFlags = Object.entries(UserFlags.FLAGS).sort(function (a, b) {
+    return Number(b[1]) - Number(a[1]);
+  });
+  ACFlags.forEach((flag) => {
+    if (processConvert - flag[1] >= 0) {
+      UFConvertResult.push(flag[0]);
+      processConvert = processConvert - flag[1];
+    }
+  });
+  return UFConvertResult;
+}
 module.exports = {
   textTruncate,
   ordinalize,
-  commatize,
+  separateNumbers,
   compactNum,
   joinArray,
   joinArrayAndLimit,
-  clean
+  clean,
+  checkLang,
+  convertUFB
 };

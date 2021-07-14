9213d9720c1a618afccf5115f6f0a6a971280e2b
A2va <49582555+A2va@users.noreply.github.com>
2021-07-14 14:59:23 +0200

    Translate changemymind and 8ball

--- src/commands/misc/8ball.js
+++ src/commands/misc/8ball.js
@@ -1,29 +1,34 @@
-const { MessageEmbed } = require('discord.js');
 
-module.exports = {
-  name: '8ball',
-  aliases: [],
-  description: '',
-  category: 'misc',
-  utilisation: '{prefix}8ball',
-  cooldown: 10,
-  nsfw: false,
-  guildOnly: false,
-  adminOnly: false,
-  ownerOnly: false,
-  permissions: [],
-  clientPermissions: ['SEND_MESSAGES', 'VIEW_CHANNEL', 'EMBED_LINKS'],
-  string: [],
+const Command = require('../../struct/Command');
+module.exports = class BallCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: '8ball',
+      aliases: [],
+      description: 'Ping!',
+      category: 'infos',
+      cooldown: 10,
+      utilisation: '{prefix}ping',
+      string: [],
+      adminOnly: false,
+    });
+  }
+  /**
+   * 
+   * @param {Client} client 
+   * @param {*} message 
+   */
   async execute(client, message, args) {
     const question = args.join(' ');
     if (!question) return message.channel.send(this.config.string[0]);
 
-    const replies = this.config.string[1]
+    const replies = message.guild.i18n.__mf("8ball.questions").split("\n");
+    //const replies = this.config.string[1]
 
     message.inlineReply(replies[Math.floor(Math.random() * replies.length)], {
         allowedMentions: {
             repliedUser: false,
         }
     });
-  },
+  }
 };

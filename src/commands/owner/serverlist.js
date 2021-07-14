fc0f02f75b02db4ae1ecf3f4b4ab98cd95677e8b
A2va <49582555+A2va@users.noreply.github.com>
2021-07-14 15:28:00 +0200

    Translate serverlist

--- src/commands/owner/serverlist.js
+++ src/commands/owner/serverlist.js
@@ -11,8 +11,8 @@ module.exports = {
     clientPermissions: ["VIEW_CHANNEL", "SEND_MESSAGES"],
     string: [],
     async execute(client, message, args) {
-      const Guilds = client.guilds.cache.array().map((G, I) => `${I + 1}. **${G.name}** - **${G.id}** - **${G.memberCount} ${this.config.string[1]}**`).join("\n");
-      if (!Guilds) return message.channel.send(this.config.string[0]);
+      const Guilds = client.guilds.cache.array().map((G, I) => `${I + 1}. **${G.name}** - **${G.id}** - **${G.memberCount} ${message.guild.i18n.__mf("serverlist.members")}**`).join("\n");
+      if (!Guilds) return message.channel.send(message.guild.i18n.__mf("serverlist.no_guild");
       return message.channel.send(Guilds, { split: { char: "\n" } }); 
     }
   };

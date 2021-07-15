8089a8afee57fd39dca96e4dd3eed3b1075a555d
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-07-15 13:22:39 +0200

    Update Guild.js

--- src/struct/Guild.js
+++ src/struct/Guild.js
@@ -2,19 +2,19 @@ const { Guild, Structures } = require('discord.js');
 const { I18n } = require('i18n');
 const path = require('path');
 
-class KiwiiGuild extends Guild{
-    constructor(client, data){
-        super(client, data);
+class KiwiiGuild extends Guild {
+  constructor(client, data) {
+    super(client, data);
 
-        this.i18n = new I18n({
-            locales: ['en','fr'],
-            directory: path.join(process.cwd(), 'locales'),
-            objectNotation: true
-          });
-        this.i18n.setLocale('en');
+    this.i18n = new I18n({
+      locales: ['en', 'fr'],
+      directory: path.join(process.cwd(), 'locales'),
+      objectNotation: true,
+    });
+    this.i18n.setLocale('en');
 
-        this.prefix = client.prefix; 
-    } 
+    this.prefix = client.prefix;
+  }
 }
 
-Structures.extend("Guild", () => KiwiiGuild);
+Structures.extend('Guild', () => KiwiiGuild);

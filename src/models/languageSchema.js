9c5a2797fd262846fd64345d4a3bca2a951e8a6f
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-07-20 22:42:05 +0200

    Update languageSchema.js

--- src/models/languageSchema.js
+++ src/models/languageSchema.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const requiredString = {
-    type: String,
-    required: true,
-}
+  type: String,
+  required: true,
+};
 
 const languageSchema = mongoose.Schema({
-    _id: requiredString,
-    language: requiredString,
+  _id: requiredString,
+  language: requiredString,
 });
 
-module.exports =  mongoose.model("languages", languageSchema)
+module.exports = mongoose.model('languages', languageSchema);

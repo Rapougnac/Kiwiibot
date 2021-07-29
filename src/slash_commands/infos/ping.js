ff3ef0d286388ba115bbd57caae250429cb9bc8c
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-07-29 21:55:40 +0200

    Moved ping.js & added defer to ping.js

---
+++ src/slash_commands/infos/ping.js
@@ -0,0 +1,32 @@
+const { Message, MessageEmbed, MessageAttachment } = require('discord.js');
+const CommandInteraction = require('../../struct/Interactions/CommandInteraction');
+const SlashCommand = require('../../struct/SlashCommand');
+const Client = require('../../struct/Client');
+module.exports = class PingSlashCommand extends SlashCommand {
+  /**
+   *@param {Client} client
+   */
+  constructor(client) {
+    super(client, {
+      name: 'ping',
+      description: 'Just a simple ping command',
+      global: false,
+    });
+  }
+  /**
+   * @param {CommandInteraction} interaction
+   * @param {Client} client
+   * @param {*} args
+   */
+  async execute(interaction, client, args) {
+    interaction.defer({ fetchReply: true }).then((message) => {
+      const ping = message.createdTimestamp - interaction.createdTimestamp;
+      const str = interaction.guild.i18n.__mf('ping.msg', {
+        pong: 'o'.repeat(Math.min(Math.round(ping / 100), 1500)),
+        ping: ping,
+        heartbeat: client.ws.ping,
+      });
+      interaction.edit(str);
+    });
+  }
+};

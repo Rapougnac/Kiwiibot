0761253b936605d5cb66d2aa332aa86a4c873052
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2021-08-16 14:43:30 +0200

    Fixed rip.js

--- src/commands/edit-images/Canvas/rip.js
+++ src/commands/edit-images/Canvas/rip.js
@@ -25,93 +25,70 @@ module.exports = class RipCommand extends Command {
    * @param {String[]} args
    */
   async execute(client, message, args) {
-    if (message.guild) {
-      const User =
-        message.mentions.members.first() ||
-        message.guild.members.cache.get(args[0]) ||
-        message.guild.members.cache.find((r) =>
-          r.user.username.toLowerCase().startsWith(args.join(' ').toLowerCase())
-        ) ||
-        message.guild.members.cache.find((r) =>
-          r.displayName.toLowerCase().startsWith(args.join(' ').toLowerCase())
+    const User =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.guild.members.cache.find((r) =>
+        r.user.username.toLowerCase().startsWith(args.join(' ').toLowerCase())
+      ) ||
+      message.guild.members.cache.find((r) =>
+        r.displayName.toLowerCase().startsWith(args.join(' ').toLowerCase())
+      );
+    if (User !== undefined) {
+      try {
+        client.utils.loader.start({
+          length: 4,
+          time: 1000,
+          allowMessage: true,
+          message,
+          deleteMessage: true,
+        });
+        message.channel.startTyping();
+        const avatarURL = User.user.displayAvatarURL({
+          format: 'png',
+          size: 512,
+        });
+        const cause = args.slice(1).join(' ');
+        const base = await loadImage(
+          join(__dirname, '..', '..', '..', 'assets', 'images', 'rip.png')
         );
-      if (User !== undefined) {
-        try {
-          message.channel.startTyping();
-          const avatarURL = User.user.displayAvatarURL({
-            format: 'png',
-            size: 512,
-          });
-          const cause = args.slice(1).join(' ');
-          const base = await loadImage(
-            join(__dirname, '..', '..', 'assets', 'images', 'rip.png')
-          );
-          const avatar = await loadImage(avatarURL);
-          const canvas = createCanvas(base.width, base.height);
-          const ctx = canvas.getContext('2d');
-          ctx.drawImage(base, 0, 0);
-          ctx.drawImage(avatar, 194, 399, 500, 500);
-          Canvas.greyscale(ctx, 194, 399, 500, 500);
-          ctx.textBaseline = 'top';
-          ctx.textAlign = 'center';
-          ctx.font = '45px Arial';
-          ctx.fillStyle = 'black';
-          ctx.fillText(User.user.username, 438, 330, 500);
-          ctx.fillText(cause, 438, 910, 500);
-          ctx.fillText('In memory of', 438, 292);
-          const att = new MessageAttachment(canvas.toBuffer(), 'rip.png');
-          message.channel.stopTyping();
-          return message.channel.send(att);
-        } catch (error) {
-          console.error(error);
-        }
-      } else {
-        try {
-          message.channel.startTyping();
-          const avatarURL = message.author.displayAvatarURL({
-            size: 512,
-            format: 'png',
-          });
-          const cause = args.join(' ');
-          //const base = await loadImage(join(__dirname, '..', '..', 'assets', 'images', 'rip2.png'));
-          const base = await loadImage(
-            join(__dirname, '..', '..', 'assets', 'images', 'rip.png')
-          );
-          //console.log(base);
-          const avatar = await loadImage(avatarURL);
-          const canvas = createCanvas(base.width, base.height);
-          const ctx = canvas.getContext('2d');
-          ctx.drawImage(base, 0, 0);
-          ctx.drawImage(avatar, 194, 399, 500, 500);
-          Canvas.greyscale(ctx, 194, 399, 500, 500);
-          ctx.textBaseline = 'top';
-          ctx.textAlign = 'center';
-          ctx.font = '45px Arial';
-          ctx.fillStyle = 'black';
-          ctx.fillText('In memory of', 438, 292);
-          ctx.fillText(message.author.username, 438, 330, 500);
-          ctx.fillStyle = 'black';
-          if (cause) ctx.fillText(cause, 438, 910, 500);
-          const att = new MessageAttachment(canvas.toBuffer(), 'rip.png');
-          message.channel.stopTyping();
-          return message.channel.send(att);
-        } catch (error) {
-          console.error(error);
-        }
+        const avatar = await loadImage(avatarURL);
+        const canvas = createCanvas(base.width, base.height);
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(base, 0, 0);
+        ctx.drawImage(avatar, 194, 399, 500, 500);
+        Canvas.greyscale(ctx, 194, 399, 500, 500);
+        ctx.textBaseline = 'top';
+        ctx.textAlign = 'center';
+        ctx.font = '45px Arial';
+        ctx.fillStyle = 'black';
+        ctx.fillText(User.user.username, 438, 330, 500);
+        ctx.fillText(cause, 438, 910, 500);
+        ctx.fillText('In memory of', 438, 292);
+        const att = new MessageAttachment(canvas.toBuffer(), 'rip.png');
+        message.channel.stopTyping();
+        return message.channel.send(att);
+      } catch (error) {
+        console.error(error);
       }
     } else {
       try {
+        client.utils.loader.start({
+          length: 4,
+          time: 1000,
+          allowMessage: true,
+          message,
+          deleteMessage: true,
+        });
         message.channel.startTyping();
         const avatarURL = message.author.displayAvatarURL({
           size: 512,
           format: 'png',
         });
         const cause = args.join(' ');
-        //const base = await loadImage(join(__dirname, '..', '..', 'assets', 'images', 'rip2.png'));
         const base = await loadImage(
-          join(__dirname, '..', '..', 'assets', 'images', 'rip.png')
+          join(__dirname, '..', '..', '..', 'assets', 'images', 'rip.png')
         );
-        //console.log(base);
         const avatar = await loadImage(avatarURL);
         const canvas = createCanvas(base.width, base.height);
         const ctx = canvas.getContext('2d');
